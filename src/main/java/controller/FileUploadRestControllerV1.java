38cc4ea104636f25874ab5601d519fbe9aab81a6
Xanarey <doctor.layout@mail.ru>
2022-11-10 18:57:48 +0300

    uploadFile

--- src/main/java/controller/FileUploadRestControllerV1.java
+++ src/main/java/controller/FileUploadRestControllerV1.java
@@ -6,7 +6,11 @@ import model.Event;
 import model.File;
 import model.User;
 import repository.EventRepo;
+import repository.FileRepo;
+import repository.UserRepo;
 import repository.hibernate.HibernateEventRepoImpl;
+import repository.hibernate.HibernateFileRepoImpl;
+import repository.hibernate.HibernateUserRepoImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -17,7 +21,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
 
 @WebServlet(name = "FileUploadServlet", urlPatterns = "/api/v1/fileUpload/*")
@@ -26,20 +29,27 @@ public class FileUploadRestControllerV1 extends HttpServlet {
 
     private final EventRepo eventRepo = new HibernateEventRepoImpl();
     private final JdbcUserRepo jdbcUserRepo = new JdbcUserRepoImpl();
+    private final FileRepo fileRepo = new HibernateFileRepoImpl();
+    private final UserRepo userRepo = new HibernateUserRepoImpl();
     private final String timeStamp = new SimpleDateFormat("dd.MM.yy").format(Calendar.getInstance().getTime());
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         User user = jdbcUserRepo.getById(Long.parseLong(request.getParameter("id")));
-
-        File file = File.builder().build();
-        file.setName(request.getParameter("name"));
-        file.setUrl("C:/Users/Пользователь/Desktop/fileStorage");
-
         Event event = Event.builder().build();
-        event.setFile(file);
+        File file = File.builder().build();
+
+        file.setName(request.getParameter("name"));
+        file.setUrl("C:/Users/Пользователь/Desktop/fileDesktop");
+        file.setEvent(event);
+
         event.setCreated(timeStamp);
-        event.setUser(new User(user.getId(), "", new ArrayList<>()));
-        eventRepo.insert(event);
+        event.setFile(file);
+        event.setUser(user);
+
+        user.getEvents().add(event);
+        userRepo.update(user);
+
+
 
         for (Part part: request.getParts()) part.write(part.getSubmittedFileName());
     }
